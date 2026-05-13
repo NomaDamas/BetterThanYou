fc4a6961cf739fcf5d55f48cccb6079e2537bd5e
minsing-jin <ironman0722@naver.com>
2026-05-14 05:44:04 +0900

    chore: update formula source sha for v0.8.20

--- Formula/better-than-you.rb
+++ Formula/better-than-you.rb
@@ -10,7 +10,7 @@ class BetterThanYou < Formula
     sha256 "16291dfe66b920a9fec201e9d02570bf3ad73a73aa6fd5c77546c17c23bd5c52"
   else
     url "https://github.com/NomaDamas/BetterThanYou/archive/refs/tags/v0.8.20.tar.gz"
-    sha256 "ed247c5f4c1bbc8c3cbb75c08ea2063b75442bbc36dc4a01b6bb1990f5207835"
+    sha256 "de0e9320606b6aa1b0d9352c5339d7e71fd4422b70590c815f19dc17f7495fe4"
 
     depends_on "rust" => :build
   end
